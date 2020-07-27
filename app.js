d018f3e14bc87d70cb92e6c2f48ab49af970e6b6
chenji <suresh.chenji@hcl.com>
2020-07-27 09:28:46 +0530

    to do list project first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const date = require(__dirname + "/views/date.js");
+
+const app = express();
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    
+    const day = date.getDate();
+    res.render('list', {listTitle: day, newListItems: items});
+
+})
+
+app.get("/about", function(req, res){
+    res.render('about')
+})
+
+app.post("/", function(req, res){
+
+    const item = req.body.newItem;
+
+    if(req.body.list === 'Work'){
+        workItems.push(item)
+        res.redirect("/work");
+
+    }else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+    
+    
+    
+})
+app.get("/work", function(req, res){
+    res.render('list', {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function(req, res){
+   const workItem = req.body.newItem;
+   workItems.push(item);
+   res.redirect("/work");
+})
+
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000!!")
+})
